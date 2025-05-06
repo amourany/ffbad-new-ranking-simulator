b6e348b2c3507bbbac4610bf21a68c9a131c971c
amourany <mourany.alex@gmail.com>
2025-05-06 11:18:38 +0200

    chore: improve seo
    (cherry picked from commit fed2723b1810bf00820a1e6c3e99fdd41a3fa98a)

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { MantineProvider } from '@mantine/core';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { theme } from './theme';
+import { HelmetProvider } from 'react-helmet-async';
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -22,11 +23,13 @@ createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<MantineProvider theme={theme}>
 			<QueryClientProvider client={queryClient}>
-				<App/>
-				<ReactQueryDevtools
-					buttonPosition="bottom-left"
-					initialIsOpen={false}
-				/>
+				<HelmetProvider>
+					<App/>
+					<ReactQueryDevtools
+						buttonPosition="bottom-left"
+						initialIsOpen={false}
+					/>
+				</HelmetProvider>
 			</QueryClientProvider>
 		</MantineProvider>
 	</StrictMode>,
