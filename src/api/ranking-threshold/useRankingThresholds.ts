f1c052fb6e2238462418e3459af3f48a6e6c4401
amourany <mourany.alex@gmail.com>
2025-06-27 09:42:14 +0200

    chore: configurating proxy

--- src/api/ranking-threshold/useRankingThresholds.ts
+++ src/api/ranking-threshold/useRankingThresholds.ts
@@ -14,6 +14,8 @@ export const useRankingThresholds = () => useQuery({
 
 const fetchRankingThresholds = async () => {
 	const rankingThresholds = await axios.get<Thresholds>(`${API_URL}/ranking-thresholds`);
+	const ip = await axios.get<string>(`${API_URL}/ip`);
+	console.log({ip: ip.data});
 
 	return rankingThresholds.data;
 };
