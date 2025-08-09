64976202e43abe7965b5f16b60b9793f771aba55
amourany <mourany.alex@gmail.com>
2025-08-09 11:00:35 +0200

    feat: augment search delay

--- src/components/SearchPlayerInput/SearchPlayerInput.tsx
+++ src/components/SearchPlayerInput/SearchPlayerInput.tsx
@@ -1,8 +1,8 @@
-import { Combobox, TextInput, useCombobox } from '@mantine/core';
-import { useTranslation } from '@hooks/useTranslation';
-import { useDebouncedCallback, useDidUpdate, useInViewport } from '@mantine/hooks';
-import { useState } from 'react';
-import { useSearchPlayer } from '@api/search-player/useSearchPlayer';
+import {Combobox, TextInput, useCombobox} from '@mantine/core';
+import {useTranslation} from '@hooks/useTranslation';
+import {useDebouncedCallback, useDidUpdate, useInViewport} from '@mantine/hooks';
+import {useState} from 'react';
+import {useSearchPlayer} from '@api/search-player/useSearchPlayer';
 import styles from './SearchPlayerInput.module.css';
 
 export type SearchPlayerInputProps = {
@@ -34,7 +34,7 @@ export const SearchPlayerInput = ({ label, onChange }: SearchPlayerInputProps) =
 			setValue(undefined);
 			combobox.closeDropdown();
 		}
-	}, 500);
+	}, 1_000);
 
 	const handleReopen = () => {
 		if (value) {
